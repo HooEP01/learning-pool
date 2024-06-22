6c9d82b22eb7070768d71efabd7835689d1b8038
HooEP01 <hooernping02@gmail.com>
2024-06-22 23:59:02 +0800

    dashboard skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,12 @@
 import SideNav from "@/components/Navigation/SideNav";
-import CardSkeleton from "@/components/Skeleton/CardSkeleton";
-import {
-  Box,
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  Container,
-  Flex,
-  Grid,
-} from "@chakra-ui/react";
+import DashboardSkeleton from "@/components/Skeleton/DashboardSkeleton";
+import { Container } from "@chakra-ui/react";
 import _ from "lodash";
 import dynamic from "next/dynamic";
 
 const Dashboard = dynamic(() => import("@/components/Dashboard"), {
   ssr: false,
-  loading: () => {
-    return (
-      <Box flex={1}>
-        <Flex direction="column" gap={4}>
-          <Breadcrumb>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
-            </BreadcrumbItem>
-
-            <BreadcrumbItem>
-              <BreadcrumbLink href="#">Tasks</BreadcrumbLink>
-            </BreadcrumbItem>
-          </Breadcrumb>
-
-          <Grid
-            templateColumns={[
-              "repeat(1, 1fr)",
-              "repeat(2, 1fr)",
-              "repeat(3, 1fr)",
-            ]}
-            gap={6}
-          >
-            {_.times(5).map(() => (
-              <CardSkeleton key={_.uniqueId("dashboard_card_skeleton_")} />
-            ))}
-          </Grid>
-        </Flex>
-      </Box>
-    );
-  },
+  loading: () => <DashboardSkeleton />,
 });
 
 export default function Home() {
