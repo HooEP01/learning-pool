0aad7cf9332740154ce66a18798c36d0e7ffbf40
HooEP01 <hooernping02@gmail.com>
2024-06-23 11:08:55 +0800

    update footer color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import CustomIcon from "./Custom/CustomIcon";
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <Box as="footer" width="full" color="white" p={3}>
+    <Box as="footer" width="full" p={3}>
       <Flex maxW="container.lg" m="auto" justify="start" align="center" gap="2">
         <CustomIcon icon={IconCoffee} />
         <Text fontSize="sm">Have a nice day | Copyright © {year} </Text>
